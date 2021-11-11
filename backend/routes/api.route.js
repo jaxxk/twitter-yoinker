ea15f060f4e1c10a110ef5b1bd1ce5239d751e24
lopez816 <lopez816@cougars.csusm.edu>
2021-11-10 20:12:13 -0800

    testing keys

--- backend/routes/api.route.js
+++ backend/routes/api.route.js
@@ -1,4 +1,12 @@
 const router = require('express').Router();
+const Twitter = require('twitter');
+ 
+const client = new Twitter({
+  consumer_key: process.env.CK,
+  consumer_secret: process.env.CS,
+  access_token_key: process.env.ATK,
+  access_token_secret: process.env.ATS
+});
 
 router.get('/', async (req, res, next) => {
   res.send({ message: 'Ok api is working 🚀' });
